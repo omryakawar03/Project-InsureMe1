d9d1bcafe69a169bfb7e90047d49a52f3d848861
Mukund Deotale <mukundeotale9860@gmail.com>
2026-01-28 10:07:43 +0530

    Update S3 bucket name in backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "<YOUR_S3_BUCKET_NAME>"
+    bucket = "aajchaprojecteksvr123hi"
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
     profile = "eks-profile"
